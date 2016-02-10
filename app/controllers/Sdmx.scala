3962a6cb941d9c9a6b0470d07766a61566306d58
bowerth <bo.werth@gmail.com>
2016-02-10 11:37:30 +0100

    remove timerange and nseries parameters from sdmx model

--- app/controllers/Sdmx.scala
+++ app/controllers/Sdmx.scala
@@ -1,16 +1,13 @@
 package controllers
 
-import java.util._
+// import java.util._
 import models.SdmxData
 import play.api.mvc._
 import scala.io.Source
 
-object Sdmx extends Controller {
 
-  // val provider = "ECB"
-  // val query = "EXR.A.USD+NZD.EUR.SP00.A"
-  // val start = "2000"
-  // val end = "2003"
+
+object Sdmx extends Controller {
 
   private val R = org.ddahl.rscala.callback.RClient()
   private val file = "R/sdmx.R"
@@ -18,13 +15,15 @@ object Sdmx extends Controller {
   private val lines = Source.fromFile(file).getLines().toList
   R.synchronized{ R eval lines.mkString("\n") }
 
-  private val errorSdmxData = SdmxData(null,null,null,null,"NA","NA")
+  // private val errorSdmxData = SdmxData(null,null,null,null,"NA","NA")
+  private val errorSdmxData = SdmxData(null,null,null,null)
 
   private def getSdmxData(provider: String, query: String, start: String, end: String) = {
     if ( provider == null ) errorSdmxData
     else try {
       val s1 = R.synchronized{R.evalS1(s"unlist(cache('$provider', '$query', '$start', '$end'))")}
-      SdmxData(provider, query, start, end, s1(1), s1(2))
+      // SdmxData(provider, query, start, end, s1(1), s1(2))
+      SdmxData(provider, query, start, end)
     } catch {
       case _: Throwable => errorSdmxData
     }
@@ -48,7 +47,9 @@ object Sdmx extends Controller {
     val (prov, qy, st, ed) = validateSdmx(provider, query, start, end)
     val sd = getSdmxData(prov, qy, st, ed)
     val sdView =
-      if ( sd.provider == null ) SdmxData(provider.toUpperCase,query,start,end,"NA","NA")
+      if ( sd.provider == null )
+        // SdmxData(provider.toUpperCase,query,start,end,"NA","NA")
+        SdmxData(provider.toUpperCase,query,start,end)
       else sd
     Ok(views.html.sdmx(sdView))
   }
