45b8a7444d34f016f62544c7b52b723d741284a6
bowerth <bo.werth@gmail.com>
2016-02-10 12:14:05 +0100

    replace R process with Scala

--- app/controllers/Sdmx.scala
+++ app/controllers/Sdmx.scala
@@ -5,23 +5,25 @@ import models.SdmxData
 import play.api.mvc._
 import scala.io.Source
 
-
+import it.bancaditalia.oss.sdmx.client.SdmxClientHandler
+import it.bancaditalia.oss.sdmx.api.PortableTimeSeries
+import java.io._
 
 object Sdmx extends Controller {
 
-  private val R = org.ddahl.rscala.callback.RClient()
-  private val file = "R/sdmx.R"
+  // private val R = org.ddahl.rscala.callback.RClient()
+  // private val file = "R/sdmx.R"
 
-  private val lines = Source.fromFile(file).getLines().toList
-  R.synchronized{ R eval lines.mkString("\n") }
+  // private val lines = Source.fromFile(file).getLines().toList
+  // R.synchronized{ R eval lines.mkString("\n") }
 
-  // private val errorSdmxData = SdmxData(null,null,null,null,"NA","NA")
+  // val errorSdmxData = SdmxData(null,null,null,null,"NA","NA")
   private val errorSdmxData = SdmxData(null,null,null,null)
 
   private def getSdmxData(provider: String, query: String, start: String, end: String) = {
     if ( provider == null ) errorSdmxData
     else try {
-      val s1 = R.synchronized{R.evalS1(s"unlist(cache('$provider', '$query', '$start', '$end'))")}
+      // val s1 = R.synchronized{R.evalS1(s"unlist(cache('$provider', '$query', '$start', '$end'))")}
       // SdmxData(provider, query, start, end, s1(1), s1(2))
       SdmxData(provider, query, start, end)
     } catch {
@@ -45,6 +47,28 @@ object Sdmx extends Controller {
 
   def main(provider: String, query: String, start: String, end: String) = Action {
     val (prov, qy, st, ed) = validateSdmx(provider, query, start, end)
+
+    // val res = SdmxClientHandler.getTimeSeries(provider, query, start, end)
+    val res = SdmxClientHandler.getTimeSeries(prov, qy, st, ed)
+    //
+    val res2 = res.toArray.
+    map(_.asInstanceOf[PortableTimeSeries])
+    //
+    val nameArray = for (ts <- res2) yield getName(ts)
+    val headerArray = "TIME_PERIOD" +: nameArray
+    //
+    val years0 = getYears(res2(0))
+    val valueArray = for (series <- res2) yield getValues(series)
+    val dataArray = years0 +: valueArray
+    //
+    val l = makeTable(data = dataArray)
+    //
+    val output = headerArray.mkString(",") + "\n" + l
+    //
+    val writer = new PrintWriter(new File("public/data/sdmx/tsdata.csv"))
+    writer.write(output)
+    writer.close()
+
     val sd = getSdmxData(prov, qy, st, ed)
     val sdView =
       if ( sd.provider == null )
@@ -70,6 +94,28 @@ object Sdmx extends Controller {
   //     }
   //   }
   // }
+
+  private def getName (tts: PortableTimeSeries): String =
+    tts.getName
+  private def getYears(tts: PortableTimeSeries): Array[String] =
+    tts.getTimeSlotsArray.map(_.toString)
+  private def getValues(tts: PortableTimeSeries): Array[String] =
+    tts.getObservationsArray.map(_.toString)
+
+  private def makeRowSeq(row: Int, data: Array[Array[String]]) =
+    for (col <- 0 to data.length-1) yield {
+      data(col)(row)
+    }
+
+  private def makeRow(row: Int, data: Array[Array[String]]) =
+    makeRowSeq(row, data).mkString(",")
+
+  private def makeTable(data: Array[Array[String]]) = {
+    val tableSeq =
+      for (row <- 0 to data(0).length-1)
+      yield makeRow(row, data)
+    tableSeq.mkString("\n")
+  }
 
 }
 
