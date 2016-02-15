834f96f41d38196d2f2b6bcde2f8ba5c49f183a0
bowerth <bo.werth@gmail.com>
2016-02-15 11:51:25 +0100

    add reset button

--- public/javascripts/custom/message.js
+++ public/javascripts/custom/message.js
@@ -1,38 +1,32 @@
 $(function() {
-    // add a click handler to the button
     
-    // $("#getMessageButton").click(function(event) {
-    //     // make an ajax get request to get the message
-    //     // jsRoutes.controllers.MessageController.getMessage().ajax({
-    //     jsRoutes.controllers.MessageController.getMessage().ajax({
-    //         success: function(data) {
-    //             console.log(data);
-    //             $(".well").append($("<h1>").text(data.value));
-    //         }
-    //     });
-    // });
-
     $("#getMessageSdmxProviderButton").click(function(event) {
         // make an ajax get request to get the message
-        // jsRoutes.controllers.MessageController.getMessage().ajax({
         jsRoutes.controllers.MessageController.getMessageSdmxProvider().ajax({
             success: function(data) {
                 console.log(data);
-                $(".well").empty();
+                $(".well").append($("<h4>").text("SDMX providers"));
                 $(".well").append($("<h5>").text(data.value));
             }
         });
     });
 
     $("#getMessageSdmxFlowButton").click(function(event) {
-        // make an ajax get request to get the message
-        // jsRoutes.controllers.MessageController.getMessage().ajax({
         var provider = $("#inputProvider").val().toUpperCase();
-        // jsRoutes.controllers.MessageController.getMessageSdmxFlow().ajax({
         jsRoutes.controllers.MessageController.getMessageSdmxFlow(provider).ajax({
             success: function(data) {
                 console.log(data);
+                $(".well").append($("<h4>").text("Flows for provider " + provider));
                 $(".well").append($("<h5>").text(data.value));
+            }
+        });
+    });
+
+    $("#clearMessageButton").click(function(event) {
+        jsRoutes.controllers.MessageController.getMessageSdmxProvider().ajax({
+            success: function(data) {
+                console.log(data);
+                $(".well").empty(); // empty div before
             }
         });
     });
